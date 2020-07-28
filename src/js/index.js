2dd22eb9c04106ef008f55c54a9222744861ba29
iv <consmebius@gmail.com>
2020-07-28 21:54:25 +0300

    fix DOMContentLoaded

--- src/js/index.js
+++ src/js/index.js
@@ -27,9 +27,10 @@ const formFooter = document.forms.form_footer;
 const formSicial = document.forms.form_social;
 const categories = document.querySelector('.categories')
 const social = document.querySelector('.social')
+console.log(social);
+
 
 categories.classList.add("preloader")
-
 window.onload = function () {
 
   // Render for Categories
@@ -37,89 +38,88 @@ window.onload = function () {
     getData(response.data, categories)
     if (response.status === 200) {
       console.log(`Get categories status = ${response.status}`);
-      // hellopreloader.style.display = "none";
       categories.classList.remove("preloader")
-
     }
   })
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  // CLICK ON SOCIAL
-  social.addEventListener('click', getEventOnIcon)
+// document.addEventListener("DOMContentLoaded", () => {
+// CLICK ON SOCIAL
+social.addEventListener('click', getEventOnIcon)
 
-  function getEventOnIcon(event) {
-    const parent = event.target.parentNode;
-    if (parent.getAttribute('id') == "menu") {
-      event.preventDefault()
-      showHideModal('.menu')
-    } else if (parent.getAttribute('id') == "map") {
-      event.preventDefault()
-      showHideModal('.map')
+function getEventOnIcon(event) {
+  const parent = event.target.parentNode;
+  if (parent.getAttribute('id') == "menu") {
+    event.preventDefault()
+    showHideModal('.menu')
+  } else if (parent.getAttribute('id') == "map") {
+    event.preventDefault()
+    showHideModal('.map')
 
-    } else if (parent.getAttribute('id') == "telephone") {
-      event.preventDefault()
-      showHideModal('.call')
+  } else if (parent.getAttribute('id') == "telephone") {
+    event.preventDefault()
+    showHideModal('.call')
+  }
+}
+
+// Render for localData
+const telephone = document.getElementById('footer-telephone')
+const email = document.getElementById('footer-email')
+const address = document.getElementById('footer-address')
+const schedule = document.getElementById('footer-schedule')
+const instagram = document.getElementById('instagram')
+const facebook = document.getElementById('facebook')
+const map = document.getElementById('social-map')
+
+telephone.textContent = localData.telephone
+telephone.setAttribute('href', `tel:${localData.telephone}`)
+email.setAttribute('href', `mailto:${localData.email}`)
+email.textContent = localData.email
+address.textContent = localData.address
+schedule.textContent = localData.schedule
+instagram.setAttribute('href', localData.instagram)
+facebook.setAttribute('href', localData.facebook)
+map.setAttribute('src', localData.map)
+
+
+// SEND CALL FORM
+formSicial.addEventListener("submit", getValueOnCall)
+formFooter.addEventListener("submit", getValueOnCall)
+
+function getValueOnCall(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const parent = e.target.parentNode;
+  const objectCall = validationForm(e);
+  if (objectCall) {
+    sendCallForm(objectCall)
+    if (parent.classList.contains('call')) {
+      parent.style.display = 'none'
     }
   }
+}
 
-  // Render for localData
-  const telephone = document.getElementById('footer-telephone')
-  const email = document.getElementById('footer-email')
-  const address = document.getElementById('footer-address')
-  const schedule = document.getElementById('footer-schedule')
-  const instagram = document.getElementById('instagram')
-  const facebook = document.getElementById('facebook')
-  const map = document.getElementById('social-map')
+// block social jumping
+window.addEventListener('scroll', function () {
+  if (window.innerWidth > 640) {
 
-  telephone.textContent = localData.telephone
-  telephone.setAttribute('href', `tel:${localData.telephone}`)
-  email.setAttribute('href', `mailto:${localData.email}`)
-  email.textContent = localData.email
-  address.textContent = localData.address
-  schedule.textContent = localData.schedule
-  instagram.setAttribute('href', localData.instagram)
-  facebook.setAttribute('href', localData.facebook)
-  map.setAttribute('src', localData.map)
-
-
-  // SEND CALL FORM
-  formSicial.addEventListener("submit", getValueOnCall)
-  formFooter.addEventListener("submit", getValueOnCall)
-
-  function getValueOnCall(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const parent = e.target.parentNode;
-    const objectCall = validationForm(e);
-    if (objectCall) {
-      sendCallForm(objectCall)
-      if (parent.classList.contains('call')) {
-        parent.style.display = 'none'
-      }
+    if (document.documentElement.scrollTop > 200) {
+      social.style.top = '5px'
+    }
+    if (document.documentElement.scrollTop < 200) {
+      social.style.top = '200px'
     }
   }
-
-  // block social jumping
-  window.addEventListener('scroll', function () {
-    if (window.innerWidth > 640) {
-
-      if (document.documentElement.scrollTop > 200) {
-        social.style.top = '5px'
-      }
-      if (document.documentElement.scrollTop < 200) {
-        social.style.top = '200px'
-      }
+  if (window.innerWidth < 640) {
+    if (document.documentElement.scrollTop > 140) {
+      social.style.top = '5px'
     }
-    if (window.innerWidth < 640) {
-      if (document.documentElement.scrollTop > 140) {
-        social.style.top = '5px'
-      }
-      if (document.documentElement.scrollTop < 200) {
-        social.style.top = '140px'
-      }
+    if (document.documentElement.scrollTop < 200) {
+      social.style.top = '140px'
     }
-  });
+  }
+});
 
-  wow.init()
-})
+wow.init();
+console.log("test ok");
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   watch: true,
   watchOptions: {
     ignored: /node_modules/,
-    poll: 1000,
+    // poll: 1000,
   },
 
   module: {
